b997372ccc8ca7102ca342a3726717fe8833ac33
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2022-03-17 15:48:45 -0700

    added code
    changed code

--- Program.cs
+++ Program.cs
@@ -3,39 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Arrays_Assignment
+namespace ConsoleApp
 {
     class Program
     {
         static void Main()
         {
-            string[] words = { "this", "computer", "is", "a", "computer" };
+            ////this is a infinite loop
+            //for (int a = 0; a < 50; a--)
+            //{
+            //    Console.WriteLine("value : {0}", a);
+            //}
+            //Console.ReadLine();
 
-            Console.WriteLine("Please select a index in the array");
+            ////fixed the loop
+            //for (int a = 0; a < 50; a++)
+            //{
+            //    Console.WriteLine("value : {0}", a);
+            //}
+            //Console.ReadLine();
 
-           var answer = Console.ReadLine();
 
-            Console.WriteLine(words[]);
+            //for (int i = 0; i <10000; i++ )
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int x = 0; x <= 500; x++)
+            //{
+            //    Console.WriteLine(x);
+            //}
 
 
+            List<string> things = new List<string>() { "car", "dog", "cat", "house", "vaccuum", "table", "cup" };
 
-            int[] numberArray = { 10, 30, 209, 49, 30 };
+            Console.WriteLine(string.Join(", ", things));
+            Console.ReadLine();
 
-            Console.WriteLine("select an index in the 4 index array");
+            Console.WriteLine("Please choose a word in the list");
+            string input = Console.ReadLine();
 
-            int input = Console.ReadLine();
-
-            if (input > numberArray.Length)
+            foreach (string thing in things)
             {
-                Console.WriteLine("that index does not exist. ");
+                if (input == things)
+                {
+                    Console.WriteLine(input);
+                }
+
+                else
+                {
+                    Console.WriteLine("sorry do not understand");
+                }
             }
-
-
-
-
-
-
         }
     }
 }
