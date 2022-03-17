8b51c131e046e1364f9f7508acb38e66f37b76e4
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2022-03-17 13:00:54 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] words = { "this", "computer", "is", "a", "computer" };
+
+            Console.WriteLine("Please select a index in the array");
+
+           var answer = Console.ReadLine();
+
+            Console.WriteLine(words[]);
+
+
+
+            int[] numberArray = { 10, 30, 209, 49, 30 };
+
+            Console.WriteLine("select an index in the 4 index array");
+
+            int input = Console.ReadLine();
+
+            if (input > numberArray.Length)
+            {
+                Console.WriteLine("that index does not exist. ");
+            }
+
+
+
+
+
+
+        }
+    }
+}
