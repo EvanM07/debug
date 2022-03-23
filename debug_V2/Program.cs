3b1ec4e4172b561fa153bed1afbfef69a6499b59
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2022-03-23 09:12:19 -0700

    version two of the debug repo
    adds the use of classes

---
+++ debug_V2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Classes_and_Methods_Assignment_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            theMaking maybe = new theMaking();
+
+            int num1;
+
+            int num2;
+
+            Console.WriteLine("Please input a number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("This second number is optional");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num2 == "")
+            {
+
+                maybe.Method(num2 = 3);
+
+
+            }
+
+            else
+            {
+                //num2 converted = Convert.ToInt32;
+                maybe.Method(num1 = 3, num2 = 4);
+            }
+
+        }
+    }
+}
