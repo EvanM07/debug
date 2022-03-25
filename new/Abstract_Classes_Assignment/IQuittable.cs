fd9414f4c64e20a429afa3183cb7aed14654c379
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2022-03-25 14:44:43 -0700

    new project
    needing debugging

---
+++ new/Abstract_Classes_Assignment/IQuittable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes_Assignment
+{
+    public interface IQuittable
+    {
+        public void Quit()
+        {
+            string answer;
+
+            Console.WriteLine("Does this employee get a raise?");
+            answer = Console.ReadLine();
+
+            if (answer == "yes")
+            {
+                Console.WriteLine("Thank you for the raise ");
+            }
+            else
+            {
+            Console.WriteLine("I quit");
+            Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ new/Abstract_Classes_Assignment/abstract_class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes_Assignment
+{
+   public class abstract_class
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public void SayName()
+        {
+            string firstName = "Sample";
+            string lastName = "Student";
+            string fullName = firstName + lastName;
+
+            Console.WriteLine("The person's full name is " + fullName);
+            Console.ReadLine();
+        }
+
+
+    }
+}
