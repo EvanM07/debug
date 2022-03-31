4bb8b91eb26b07ad159098b3650519030b7bd518
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2022-03-31 14:14:03 -0700

    adding for debug
    adding for debug

---
+++ debug_V2/Generics_Assignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+    public class Employee <t>
+    {
+        public List<t> Things  { get; set; }
+
+
+    }
+}
---
+++ debug_V2/Generics_Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+             Employee<string> employeeOne = new Employee<string>();
+             employeeOne.Things = new List<string>() { "apple", "pear", "orange","cat" };
+
+
+            Employee<int> employeeTwo = new Employee<int>();
+            employeeTwo.Things = new List<int>() { 1, 3, 4, 5 };
+
+            for (var i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    employeeOne.Things = (Things)i;
+
+
+
+                }
+            }
+        }
+    }
+}
